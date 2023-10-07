cdc2f254f9c19edcd2739cb9298c7a486aa4c466
Bryan Chen <60024199+bryancwh@users.noreply.github.com>
2023-10-07 10:59:28 +0800

    added nginx

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 
 const db = mysql.createPool({
     host: 'mysql_db', // the host name MYSQL_DATABASE: node_mysql
-    user: 'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
-    password: 'MYSQL_PASSWORD', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
+    user: 'stellar', // database user MYSQL_USER: MYSQL_USER
+    password: 'password', // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
     database: 'books' // database name MYSQL_HOST_IP: mysql_db
 })
 
